820eab3b523cb2bbe6d5bba206d762843fcdf174
danielrgb2 <danielrgb2@gmail.com>
2022-03-05 17:10:36 -0300

    inicio do  projeto

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import avatar from '../img/avatar'
+
+function Navbar() {
+    return(
+        <div className="NavBar">
+            <div className="profile">
+                <img src={avatar} alt
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/homepage.tsx
@@ -0,0 +1,9 @@
+function homepage() {
+    return (
+        <div>
+            <h1>HomePagem</h1>
+        </div>
+    );
+}
+
+export default homepage;
