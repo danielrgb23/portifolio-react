f0d75dc3979af689b19a398b7dfa4c1ac07e1136
danielrgb2 <danielrgb2@gmail.com>
2022-03-06 14:59:17 -0300

    header page-construção

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
-import avatar from '../img/avatar'
+//@ts-ignore
+import avatar from '../img/avatar.png';
+import { NavLink } from 'react-router-dom';
 
-function Navbar() {
-    return(
+function Navbar() { 
+    return(     
         <div className="NavBar">
-            <div className="profile">
-                <img src={avatar} alt
-            </div>
+            <nav className="nav">
+                <div className="profile">
+                    <img src={ avatar } alt=""/>
+                </div>
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        <NavLink to="/" exact actionClassName="active">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/About" exact actionClassName="active">
+                            About
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/Portifolios" exact actionClassName="active">
+                            Portifolios
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/Blogs" exact actionClassName="active">
+                            Blogs
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/Contact" exact actionClassName="active">
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>  
+                 <footer className="footer">
+                    <p>
+                            @2022 copyright | Lorem ipsum
+                    </p>
+                </footer>
+            </nav>
         </div>
     )
 }
+
+export default Navbar;
